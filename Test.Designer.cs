f590a1791e4c87fec4b2fcc1f1eaa4ed32808c73
hoand <hoand@iotlink.com.vn>
2023-06-01 17:31:38 +0700

    + Add project

---
+++ Test.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace FullMin
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl_test = new OpenTK.GLControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // glControl_test
+            // 
+            this.glControl_test.BackColor = System.Drawing.Color.Black;
+            this.glControl_test.Location = new System.Drawing.Point(194, 53);
+            this.glControl_test.Name = "glControl_test";
+            this.glControl_test.Size = new System.Drawing.Size(856, 504);
+            this.glControl_test.TabIndex = 0;
+            this.glControl_test.VSync = false;
+            this.glControl_test.Load += new System.EventHandler(this.glControl_test_Load);
+            this.glControl_test.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl_test_Paint);
+            this.glControl_test.Resize += new System.EventHandler(this.glControl_test_Resize);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 589);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.glControl_test);
+            this.Name = "Test";
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Test_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl_test;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+
+namespace FullMin
+{
+    public partial class Test : Form
+    {
+        private float hue; // Biến thay đổi màu
+        private Bitmap[] frames; // Mảng chứa các hình ảnh BMP
+        private float rotationAngle; // Góc quay của hình tròn
+        private float rotationSpeed; // Tốc độ quay của hình tròn
+        private int currentFrameIndex = 0; // Chỉ số hình ảnh hiện tại
+        public Test()
+        {
+            frames = LoadFrames();
+            InitializeComponent();
+            // Thiết lập các thuộc tính OpenGLControl
+            
+        }
+
+        private void Test_Load(object sender, EventArgs e)
+        {
+            //GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            UpdateAnimation();
+
+        }
+        private void Draw()
+        {
+            // Xóa màn hình
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            // Vẽ hình tròn
+            GL.Color3(Color.Red);  // Đặt màu cho hình tròn
+            GL.Begin(BeginMode.Polygon);
+            for (int i = 0; i < 360; i++)
+            {
+                double angle = i * Math.PI / 180.0;
+                double x = Math.Cos(angle);
+                double y = Math.Sin(angle);
+                GL.Vertex2(x, y);
+            }
+            GL.End();
+
+            // Hiển thị hình tròn lên OpenGLControl
+            glControl_test.SwapBuffers();
+        }
+
+        private void glControl_test_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void glControl_test_Resize(object sender, EventArgs e)
+        {
+            int w = glControl_test.Width;
+            int h = glControl_test.Height;
+            glControl_test.MakeCurrent();
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            //GL.ClearColor(Color.Red);
+            GL.Ortho(-w / 2, w / 2, -h / 2, h / 2, -1, 1);
+            GL.Viewport(0, 0, w, h);
+            GL.End();
+            glControl_test.SwapBuffers();
+        }
+
+        private void glControl_test_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.MatrixMode(MatrixMode.Projection);
+            //GL.LoadIdentity();
+           // GL.Ortho(-1, 1, -1, 1, -1, 1);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            drawCircle(5,5,5);
+            drawCircle(5, 5, 20);
+            drawCircle(5, 5, 35);
+            //point();
+            glControl_test.SwapBuffers();
+        }
+        //private void Draw_clock()
+        //{
+        //    drawCircle(80);//80 is radius of the circle
+        //    //Draw_digit();
+        //}
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            // Cập nhật giá trị màu
+            hue += 0.01f;
+            if (hue > 1)
+                hue = 0;
+
+            glControl_test.Invalidate(); // Yêu cầu vẽ lại control
+        }
+        private void drawCircle(float radius,float x, float y)
+        {
+            // Chọn hình ảnh hiện tại để vẽ
+            //Bitmap currentFrame = frames[currentFrameIndex];
+
+            //// Chuyển đổi hình ảnh BMP thành dữ liệu pixel dùng cho OpenGL texture
+            //BitmapData bitmapData = currentFrame.LockBits(new Rectangle(0, 0, currentFrame.Width, currentFrame.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            //GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bitmapData.Width, bitmapData.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bitmapData.Scan0);
+            //currentFrame.UnlockBits(bitmapData);
+
+            //GL.Color3(Color.White);
+            GL.Begin(BeginMode.TriangleFan);
+            GL.Color3(ColorFromHSV(hue, 1, 1)); // Sử dụng màu từ giá trị HSV
+
+            for (int i = 0; i < 360; i++)
+            {
+                double degInRad = i * 3.1416 / 180;
+                GL.Vertex2(Math.Cos(degInRad) * radius + x , Math.Sin(degInRad) * radius + y);
+            }
+            GL.End();
+            GL.PopMatrix();
+        }
+        private Color ColorFromHSV(float hue, float saturation, float value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            float f = hue / 60 - (float)Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+        private void point()
+        {
+
+            float pointSize = 10f; // Kích thước của điểm
+            float borderRadius = 50f; // Bán kính của border radius
+
+            // Chọn hình ảnh hiện tại để vẽ
+            Bitmap currentFrame = frames[currentFrameIndex];
+
+            // Chuyển đổi hình ảnh BMP thành dữ liệu pixel dùng cho OpenGL texture
+            BitmapData bitmapData = currentFrame.LockBits(new Rectangle(0, 0, currentFrame.Width, currentFrame.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bitmapData.Width, bitmapData.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bitmapData.Scan0);
+            currentFrame.UnlockBits(bitmapData);
+
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Vertex2(-borderRadius, -borderRadius); // Góc dưới bên trái
+            GL.Vertex2(-borderRadius, borderRadius); // Góc trên bên trái
+            GL.Vertex2(borderRadius, borderRadius); // Góc trên bên phải
+            GL.Vertex2(borderRadius, -borderRadius); // Góc dưới bên phải
+            GL.End();
+
+            GL.PointSize(pointSize); // Đặt kích thước của điểm
+            GL.Color3(Color.Red);
+            GL.Begin(PrimitiveType.Points);
+            GL.Vertex2(0, 0); // Tọa độ của điểm
+            GL.End();
+
+            //GL.PointSize(pointSize);
+            //GL.Color3(Color.Red);  // Đặt màu cho điểm
+            //GL.Begin(BeginMode.Points);
+            //GL.Vertex2(0, 0);  // Tọa độ của điểm
+            //GL.End();
+        }
+        private Bitmap[] LoadFrames()
+        {
+            // Đọc các hình ảnh BMP từ thư mục hoặc tệp tin
+            // Và trả về mảng các hình ảnh đã đọc
+
+            // Ví dụ:
+            string[] filePaths = Directory.GetFiles("image", "2_.bmp");
+            List<Bitmap> frames = new List<Bitmap>();
+
+            foreach (string filePath in filePaths)
+            {
+                Bitmap frame = new Bitmap(filePath);
+                frames.Add(frame);
+            }
+
+            return frames.ToArray();
+        }
+        private void UpdateAnimation()
+        {
+            // Cập nhật góc quay của hình tròn
+            rotationAngle += 2;
+
+            // Kiểm tra nếu đã đến hình ảnh cuối cùng trong mảng
+            if (currentFrameIndex >= frames.Length - 1)
+            {
+                // Reset lại chỉ số hình ảnh để quay trở lại hình ảnh đầu tiên
+                currentFrameIndex = 0;
+            }
+            else
+            {
+                // Tăng chỉ số hình ảnh hiện tại
+                currentFrameIndex++;
+            }
+        }
+    }
+}
